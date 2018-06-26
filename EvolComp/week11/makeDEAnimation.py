e22fea53f6e08c5d7df18768c082d4c4f59a9356
sekishin <sekishin1214@gmail.com>
2018-06-26 17:10:33 +0900

    make animation progral not completed

--- EvolComp/week11/makeDEAnimation.py
+++ EvolComp/week11/makeDEAnimation.py
@@ -1,3 +1,4 @@
+from myDE import sphere, rastrigin, myDE, simulation
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from tqdm import tqdm
@@ -7,20 +8,13 @@ def makeAnimation(data, save):
     ax = fig.add_subplot(1,1,1)
     ims = []
     pbar = tqdm(total=len(data))
-    t = 1
     for d in data:
-        x1 = [i[0] for i in d]
-        x2 = [i[1] for i in d]
-        print(len(x1),len(x2))
-        ax.set_title(save+" t = "+str(t))
-        t += 1
-        im = ax.scatter(x1,x2, c='red', s=20, marker='o')
+        for i in d:
+          im = plt.scatter(i[0],i[1],c='red')
         ims.append([im])
         pbar.update(1)
     pbar.close()
-    ax.set_xlim(-5, 5)
-    ax.set_ylim(-5, 5)
-    ax.grid()
+    ax.set_title(save)
     ax.set_xlabel('x1')
     ax.set_ylabel('x2')
     anime = animation.ArtistAnimation(fig,ims,interval=100)
--- EvolComp/week11/myDE.py
+++ EvolComp/week11/myDE.py
@@ -3,7 +3,6 @@ from tqdm import tqdm
 from statistics import mean, variance
 import math
 import numpy as np
-from makePSOAnimation import makeAnimation
 
 def sphere(x):
     result = 0
@@ -17,60 +16,72 @@ def rastrigin(x):
         result += (elem**2 - 10 * math.cos(2 * math.pi * elem) + 10)
     return result
 
-def next_v(v, p, x, g):
-    c = 1.494
-    w = 0.729
-    r1 = random.random()
-    r2 = random.random()
-    n = w * v + c * r1 * (p-x) + c * r2 * (g-x)
-    return n
+def next_v(a, b, c):
+    Fw = 0.5
+    return a + Fw * (b - c)
 
-def myPSO(D, func):
+def extract_three(i, x):
+    while True:
+        tmp = random.sample(x, 3)
+        if i not in tmp: break
+    return tmp
+
+def myDE(D, func):
     M = 30
     t_max = 1000
-    Cr = 10**-5
+    Cr = 0.9
     t = 0
+    f_end = 1**-5
     x_min = -5
     x_max = 5
     x = [[random.uniform(x_max,x_min) for i in range(D)] for j in range(M)] 
-    v = [[0 for i in range(D)] for j in range(M)] 
-    f = [0 for i in range(M)]
-    f_gbest = float('inf')
-    x_gbest = [0 for i in range(D)]
-    f_pbest = [float('inf') for i in range(M)] 
-    x_pbest = [[10 for i in range(D)] for j in range(M)]
-    x_log = []
+    x_new = [[0 for i in range(D)] for j in range(M)] 
+    v = [0 for i in range(D)] 
+    u = [0 for i in range(D)]
+    f = [func(i) for i in x]
+    f_tmp = 0
+    f_best = float('inf')
+    x_best = [0 for i in range(D)]
+    
     while t < t_max:
         t = t + 1
-        x_log.append(x_pbest)
         for i in range(M):
-            f[i] = func(x[i])
-            if f[i] < f_pbest[i]:
-                f_pbest[i] = f[i]
+            a, b, c = extract_three(i, x)
+            for d in range(D):
+                v[d] = next_v(a[d], b[d], c[d])
+            Jr = random.randrange(D)
+            for d in range(D):
+                ri = random.random()
+                if ri < Cr or d == Jr:
+                    u[d] = v[d]
+                else:
+                    u[d] = x[i][d]
+            f_tmp = func(u)
+            if f_tmp < f[i]:
+                f[i] = f_tmp
                 for d in range(D):
-                    x_pbest[i][d] = x[i][d]
-                if f_pbest[i] < f_gbest:
-                    f_gbest = f_pbest[i]
+                    x_new[i][d] = u[d]
+                if f_tmp < f_best:
+                    f_best = f_tmp
                     for d in range(D):
-                        x_gbest[d] = x_pbest[i][d]
-        if f_gbest < Cr: break
+                        x_best[d] = u[d]
+            else:
+                for d in range(D):
+                    x_new[i][d] = x[i][d]
         for i in range(M):
             for d in range(D):
-                v[i][d] = next_v(v[i][d], x_pbest[i][d], x[i][d], x_gbest[d])
-                x[i][d] += v[i][d]
-    #print("loop time = {}".format(t))
-    #print("best function value = {}".format(f_gbest))
-    #print("answer = {}".format(x_gbest))
-    return t, f_gbest, x_log
+                x[i][d] = x_new[i][d]
+        if f_best < f_end:
+            break
+    return t, f_best, x
 
 def simulation(D,func):
     time = []
     f_value = []
     x_position = []
-    title = str(D)+func.__name__
     pbar = tqdm(total=100)
     for i in range(100):
-        t, f, x = myPSO(D, func)
+        t, f, x = myDE(D, func)
         time.append(t)
         f_value.append(f)
         x_position.append(x)
@@ -78,7 +89,6 @@ def simulation(D,func):
     pbar.close()
     std = np.array(f_value)
     print(D, func.__name__, mean(f_value), np.var(std,ddof=0), np.var(std,ddof=1), mean(time))
-    makeAnimation(x_position, title)
 
 if __name__ == "__main__":
     print("D, function, f-value mean, f-value var, f-value std-var, loop time mean")
