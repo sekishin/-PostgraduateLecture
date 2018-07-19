8edfbb9967918a8ab1af603549175b41590f4300
sekishin <s14t242@stmail.eng.kagawa-u.ac.jp>
2018-07-19 15:37:24 +0900

    add fix graho

--- EvolComp/report/makeGraph.py
+++ EvolComp/report/makeGraph.py
@@ -9,11 +9,14 @@ def makeGraph(data, save, label):
     ax.set_xlabel("t")
     ax.set_ylabel("f_best")
     ax.set_title(save)
-    for d, l in zip(data, label):
+    color = ["red","green","blue"]
+    for d, l, c in zip(data, label, color):
         x = [i for i in range(1,len(d)+1)]
-        plt.plot(x, d, label=l)
+        plt.plot(x, d, color=c, label=l)
+    plt.legend(loc=2)
     plt.savefig(save+".png")
+    plt.close()
 
 if __name__ == "__main__":
-    data = [i for i in range(100)]
-    makeGraph(data, "test")
+    data = [[i for i in range(100)]]
+    makeGraph(data, "test", "test")
