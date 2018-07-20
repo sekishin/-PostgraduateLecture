3d708284e121772a1e950a81aae5e58e82dd03d4
User S18g471 <s18g471@lecweb.eng.kagawa-u.ac.jp>
2018-07-20 12:09:35 +0900

    hoge

--- ConProg/kadai1/server.c
+++ ConProg/kadai1/server.c
@@ -6,6 +6,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <netdb.h>
 
 #define SERV_TCP_PORT 20000
@@ -27,22 +29,22 @@ int main(int argc, char *argv[])
     struct sockaddr_in serv_addr, cli_addr;
     socklen_t cli_len = sizeof(cli_addr);
     int pid;
-    char buf[RECORD_SIZE];
-    int i, size;
-    int fd;
-    char *old_idnt;
+    char ch;
+    char buff[RECORD_SIZE];
+    int chnum = 0;
+    int status;
 
     if (argc < 1) {
         err_msg("error : no file_name");
     }
     
     // ファイル作成
-    strcopy(file_name, argv[1]);
+    strcpy(file_name, argv[1]);
     fp = fopen(file_name, "w");
     if (fp == NULL) {
         err_msg("server : can't open file");
     }
-    fclose(fp)
+    fclose(fp);
     
     // ソケット生成
     port_no = atoi(file_name);
@@ -54,7 +56,7 @@ int main(int argc, char *argv[])
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(port_np);
+    serv_addr.sin_port = htons(port_no);
     
     // ソケット登録    
     if (bind(sockid, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
@@ -63,11 +65,66 @@ int main(int argc, char *argv[])
     
     // ソケット待ち受け
     if (listen(sockid, 5) == -1) {
-        err_msg("server : listen fails")
+        err_msg("server : listen fails");
     }
 
     while (1) {
-        break;
+        // 子プロセスの生成
+        if (chnum) {
+            while(chnum && ((pid = waitpid(-1, &status, WNOHANG)) > 0)) {
+                fprintf(stderr, "Terminate child process: %d\n", pid);
+                chnum--;
+            }
+        }
+        // コネクション受付
+        if ((sockfd = accept(sockid, (struct sockaddr *)&cli_addr, &cli_len)) < 0) {
+            close(sockid);
+            fprintf(stderr, "server: can't accept");
+            break;
+        }
+        // fork実行
+        pid = fork();
+        if (pid < 0) {  // fork error
+            close(sockfd);
+            close(sockid);
+            break;
+        } else if (pid > 0) {   // parent process
+            // 通信用のソケットを閉じ、次のループへ
+            close(sockfd);
+            chnum++;
+            continue;
+        }
+        
+        // child process
+        pid = getpid();
+        fprintf(stderr, "\nI am child process&d\n",pid);
+        if (pid == 0) {
+            close(sockid);  // コネクション待ちのソケットを閉じる
+            fp = fopen(file_name, O_RDWR | O_CREAT, 0644);  // ファイルオープン
+            if (fp == NULL) err_msg("server: can't open file");
+            if (flock(fp, LOCK_EX) == -1) { // ファイルロック
+                fclose(fp)
+                err_msg("server: can't lock file");
+            }
+            fseek(fp, 0, SEEK_END);
+            long size = ftell(fp);  // ファイルサイズ取得
+            if (size == 0) {
+                //メッセージを 0 NONE NONE に
+                sprintf(buff, "0 NONE NONE");
+            } else {
+                // ファイル末尾のメッセージに
+            }
+            // クライアントからメッセージが届くまで待つ
+            // 届いたメッセージをファイルに追記
+            // ファイルのロックを解除
+            // ファイルクローズ
+            // プロセス修了
+
+        }
+    }
+    
+    while (chnum) {
+        if (waitpid(-1, &status, WNOHANG) > 0) chnum--;
     }
 
     return 0;
