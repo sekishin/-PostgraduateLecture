6c0afdf83ad1fe656217fd746e4905fb2eac1ea3
User S18g471 <s18g471@lecweb.eng.kagawa-u.ac.jp>
2018-07-25 15:50:15 +0900

    conprog kadai1 program

--- ConProg/kadai1/client.c
+++ ConProg/kadai1/client.c
@@ -8,88 +8,84 @@
 #include <unistd.h>
 #include <netdb.h>
 
-#define	SERV_TCP_PORT 20000
+#define SERV_TCP_PORT 20000
+#define RECORD_SIZE 100
 
 void err_msg(char *msg)
 {
-  perror(msg);
-  exit(1);
+    puts(msg);
+    exit(1);
 }
 
 int main(int argc, char *argv[])
 {
-  int port_no;
-  int ipaddr;
-  int sockfd;
-  struct sockaddr_in serv_addr;
-  int rflag, wflag, endflag;
-  char sch, rch;
-  struct hostent *retrieve;
-  
-  if (argc < 2) {
-    fprintf(stderr,"Usage: client serv_addr [port_no]\n");
-    exit(1);
-  }
-  port_no = (argc > 2) ? atoi(argv[2]) : SERV_TCP_PORT;
-  if ((retrieve = gethostbyname(argv[1])) == NULL) {
-    printf("Unknown host name: %s\n", argv[1]);
-    exit(1);
-  }
-  ipaddr = *(unsigned int *)(retrieve->h_addr_list[0]);
-  printf("%d.%d.%d.%d (%d) にアクセスします．Ctrl-A を押すと終了します．\n",
-	 ipaddr & 0xff, (ipaddr >> 8) & 0xff, (ipaddr >> 16) & 0xff, (ipaddr >> 24) & 0xff,
-	 port_no);
-  bzero((char *)&serv_addr, sizeof(serv_addr));
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_addr.s_addr = *(unsigned int *)(retrieve->h_addr_list[0]);
-  serv_addr.sin_port = htons(port_no);
-  
-  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    err_msg("client: can't open datastream socket");
-  }
-  
-  if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-    err_msg("client: can't connect server address");
-  }
+    int port_no;
+    int ipaddr;
+    int sockfd;
+    struct sockaddr_in serv_addr;
+    struct hostent *retrieve;
+    int loop_num;   // 繰り返し回数
+    char idnt[40]; //  識別子
+    char idnt1[40]; // 識別子1
+    char idnt2[40]; // 識別子2
+    int num;
+    int i; // 反復変数 
+    char inbuff[RECORD_SIZE];   // 受信メッセージ用バッファ
+    char outbuff[RECORD_SIZE];  // 送信メッセージ用バッファ
+    
+    if (argc < 2) {
+        err_msg("client: input server name");
+    } else if (argc < 3) {
+        err_msg("client: input id sequence");
+    } else if (argc < 4) {
+        err_msg("client: input loop time");
+    } else if (strlen(argv[2]) > 35) {
+        err_msg("client: too long id sequence");
+    }
 
-  fcntl(fileno(stdin), F_SETFL, O_NONBLOCK);
-  fcntl(fileno(stdout), F_SETFL, O_NONBLOCK);
-  fcntl(sockfd, F_SETFL, O_NONBLOCK);
-  rflag = 1; wflag = 0;
-  while (1) {
-    if (rflag == 1) {
-      if (read(fileno(stdin), &sch, 1) == 1) {
-	if (sch == '\001') {
-	  endflag = 0;
-	  break;	/* ^A */
-	}
-	rflag = 0;
-      }
+    port_no = SERV_TCP_PORT;
+    strcpy(idnt, argv[2]);
+    loop_num = atoi(argv[3]);
+    if ((retrieve = gethostbyname(argv[1])) == NULL) {
+        err_msg("client: unknown host name");
+
     }
-    if (rflag == 0) {
-      if (write(sockfd, &sch, 1) == 1) {
-	rflag = 1;
-      }
+    ipaddr = *(unsigned int *)(retrieve->h_addr_list[0]);
+    printf("%d.%d.%d.%d (%d) にアクセスします．Ctrl-A を押すと終了します．\n",ipaddr & 0xff, (ipaddr >> 8) & 0xff, (ipaddr >> 16) & 0xff, (ipaddr >> 24) & 0xff,port_no);
+  
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = *(unsigned int *)(retrieve->h_addr_list[0]);
+    serv_addr.sin_port = htons(port_no);
+  
+  
+
+    for (i = 0; i < loop_num; i++) {
+        // サーバに接続
+        if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+            err_msg("client: can't open datastream socket");
+        }
+        if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+            err_msg("client: can't connect server address");
+        }
+        // メッセージを受信
+        read(sockfd, inbuff, RECORD_SIZE);
+        // メッセージを表示
+        printf("from server: %s\n", inbuff);
+        sscanf(inbuff, "%d %s %s", &num, idnt1, idnt2);
+        // 一時待機
+        sleep(1);
+        // メッセージを送信
+        memset(outbuff, 0x0, RECORD_SIZE);
+        snprintf(outbuff, RECORD_SIZE, "%d %s %s", num+1, idnt, idnt1);
+        printf("send server: %s\n", outbuff);
+        write(sockfd, outbuff, RECORD_SIZE);
+        //  コネクション切断
+        close(sockfd);
+        // 一時待機
+        sleep(1);
     }
-    if (wflag == 0) {
-      if (read(sockfd, &rch, 1) == 1) {
-	if (rch == '\001') {
-	  endflag = 1;
-	  break;	/* ^A */
-	}
-	wflag = 1;
-      }
-    }
-    if (wflag == 1) {
-      if (write(fileno(stdout), &rch, 1) == 1) {
-	wflag = 0;
-      }
-    }
-  }
-  if (endflag == 0) {
-    while (write(sockfd, &sch, 1) != 1);
-  }
-  write(fileno(stdout), "\nEnd.\n", strlen("\nEnd.\n")+1);
-  close(sockfd);
-  return 0;
+    //----
+    close(sockfd);
+    return 0;
 }
--- ConProg/kadai1/server.c
+++ ConProg/kadai1/server.c
@@ -9,45 +9,46 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <netdb.h>
+#include <sys/file.h>
 
 #define SERV_TCP_PORT 20000
 #define RECORD_SIZE 100
 
 void err_msg(char *msg)
 {
-    perror(msg);
+    puts(msg);
     exit(1);
 }
 
 int main(int argc, char *argv[])
 {
-    char *file_name;    // 対象ファイル
-    FILE *fp;   // ファイルポインタ
+    char file_name[64];    // 対象ファイル
+    int fp;   // ファイルポインタ
     int port_no;    // ポート番号
     int sockid; // ソケットIDa
     int sockfd;
     struct sockaddr_in serv_addr, cli_addr;
     socklen_t cli_len = sizeof(cli_addr);
     int pid;
-    char ch;
-    char buff[RECORD_SIZE];
+    char outbuff[RECORD_SIZE];  // 送信メッセージ用バッファ
+    char inbuff[RECORD_SIZE];   // 受信メッセージ用バッファ
     int chnum = 0;
+    int size;
     int status;
+    int num;
+    char idnt1[40]; // 識別子1
+    char idnt2[40]; // 識別子2
 
-    if (argc < 1) {
+    if (argc <= 1) {
         err_msg("error : no file_name");
     }
     
     // ファイル作成
     strcpy(file_name, argv[1]);
-    fp = fopen(file_name, "w");
-    if (fp == NULL) {
-        err_msg("server : can't open file");
-    }
-    fclose(fp);
+    fclose(fopen(file_name, "w"));
     
     // ソケット生成
-    port_no = atoi(file_name);
+    port_no = SERV_TCP_PORT;
     printf("待ち受けポートは %d 番です\n", port_no);
     if ((sockid = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         err_msg("server : can't open datastream socket");
@@ -72,14 +73,14 @@ int main(int argc, char *argv[])
         // 子プロセスの生成
         if (chnum) {
             while(chnum && ((pid = waitpid(-1, &status, WNOHANG)) > 0)) {
-                fprintf(stderr, "Terminate child process: %d\n", pid);
+                //fprintf(stderr, "Terminate child process: %d\n", pid);
                 chnum--;
             }
         }
         // コネクション受付
         if ((sockfd = accept(sockid, (struct sockaddr *)&cli_addr, &cli_len)) < 0) {
             close(sockid);
-            fprintf(stderr, "server: can't accept");
+            //fprintf(stderr, "server: can't accept");
             break;
         }
         // fork実行
@@ -96,36 +97,42 @@ int main(int argc, char *argv[])
         }
         
         // child process
-        pid = getpid();
-        fprintf(stderr, "\nI am child process&d\n",pid);
+        fprintf(stderr, "\nI am child process%d\n", getpid());
         if (pid == 0) {
             close(sockid);  // コネクション待ちのソケットを閉じる
-            fp = fopen(file_name, O_RDWR | O_CREAT, 0644);  // ファイルオープン
-            if (fp == NULL) err_msg("server: can't open file");
+            fp = open(file_name, O_RDWR | O_CREAT, 0644);  // ファイルオープン
+#ifdef WITH_FLOCK
             if (flock(fp, LOCK_EX) == -1) { // ファイルロック
-                fclose(fp)
+                close(fp)
                 err_msg("server: can't lock file");
             }
-            fseek(fp, 0, SEEK_END);
-            long size = ftell(fp);  // ファイルサイズ取得
+#endif
+            lseek(fp, -(RECORD_SIZE+1), SEEK_END);
+            size = read(fp, inbuff, RECORD_SIZE);  // ファイルサイズ取得(この時点でファイル末尾のデータはinbuffに格納される)
+            memset(outbuff, 0x0, RECORD_SIZE);  // パティング
             if (size == 0) {
                 //メッセージを 0 NONE NONE に
-                sprintf(buff, "0 NONE NONE");
+                sprintf(outbuff, "0 NONE NONE");
             } else {
                 // ファイル末尾のメッセージに
+                sprintf(outbuff, "%s", inbuff);
             }
-            // クライアントからメッセージが届くまで待つ
+            printf("send client: %s\n", outbuff);
+            write(sockfd, outbuff, RECORD_SIZE);  // メッセージをクライアントに送る
+            read(sockfd, inbuff, RECORD_SIZE);  // クライアントからメッセージが届くまで待つ
             // 届いたメッセージをファイルに追記
-            // ファイルのロックを解除
-            // ファイルクローズ
-            // プロセス修了
+            printf("from client: %s\n", inbuff);
+            lseek(fp, 0, SEEK_END);
+            write(fp, inbuff, RECORD_SIZE);
+            write(fp, "\n", 1);
+#ifdef WITH_FLOCK
+            flock(fp, LOCK_UN);  // ファイルのロックを解除
+#endif
+            close(fp); // ファイルクローズ
+            close(sockfd);  // プロセス修了
 
         }
     }
     
-    while (chnum) {
-        if (waitpid(-1, &status, WNOHANG) > 0) chnum--;
-    }
-
     return 0;
 }
