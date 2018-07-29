d936e25e6ba7cfd30c3a9b509bfb2f178d0e5508
User S18g471 <s18g471@lecweb.eng.kagawa-u.ac.jp>
2018-07-29 17:44:07 +0900

    add corrected result data

--- ConProg/kadai2/CorrectData.c
+++ ConProg/kadai2/CorrectData.c
@@ -14,9 +14,7 @@ int main(int argc, char const *argv[]) {
   char buff[256];
   char *p;
   if (argc < 2) error("input log file name");
-  puts(argv[1]);
   strcpy(filename, argv[1]);
-  puts(filename);
   fp = fopen(filename, "r");
   if (fp == NULL) error("illigal file name");
   while(fgets(buff, sizeof(buff), fp) != NULL) {
